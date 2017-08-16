56bdcb62aecdc6d77b8a67c97d09f1b3080e4ae1
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-08-16 00:21:10 -0300

    Font sizes adjust based on question

---
+++ Assets/Scripts/Code/FixUITextSize.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class FixUITextSize : MonoBehaviour
+{
+    public int incrementSize = 20;
+
+    void Start()
+    {
+        int currentSize = GetComponent<Text>().text.Length;
+        if (currentSize < 28)
+        {
+            GetComponent<Text>().fontSize = 80;
+        }
+        else if (currentSize < 43)
+        {
+            GetComponent<Text>().fontSize = 65;
+        }
+        else if (currentSize < 58)
+        {
+            GetComponent<Text>().fontSize = 50;
+        }
+        else if (currentSize < 73)
+        {
+            GetComponent<Text>().fontSize = 40;
+        }
+    }
+}
