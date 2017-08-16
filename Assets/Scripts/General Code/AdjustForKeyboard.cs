8d9ca4bb25a3885a0d0a27d1c689de2f44a8e84a
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-08-16 11:17:02 -0300

    Added basics of moving screen for keyboard

---
+++ Assets/Scripts/General Code/AdjustForKeyboard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustForKeyboard : MonoBehaviour
+{
+    private Vector3 oriPos;
+    private bool keyboardWasVisible = false;
+
+    private void Start()
+    {
+        oriPos = this.transform.position;
+    }
+
+    private void Update()
+    {
+        if (TouchScreenKeyboard.visible && keyboardWasVisible == false)
+        {
+            this.transform.position += new Vector3(0, TouchScreenKeyboard.area.height, 0);
+            keyboardWasVisible = true;
+        }
+        else if (keyboardWasVisible)
+        {
+            this.transform.position = oriPos;
+            keyboardWasVisible = false;
+        }
+    }
+
+}
